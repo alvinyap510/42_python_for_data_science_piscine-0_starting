4deb60468d8a98c7394e6ec15f849c43af546589
Alvin Yap <alyap@u91z04s04.42kl.edu.my>
2023-10-01 23:07:44 +0800

    chore: format flake8

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -4,5 +4,9 @@ current_datetime = datetime.now()
 unix_timestamp = current_datetime.timestamp()
 
 print(
-    f"Seconds since January 1, 1970: {format(unix_timestamp, ',.4f')} or {format(unix_timestamp, '.2e')} in scientific notation")
+    "Seconds since January 1, 1970: " +
+    f"{format(unix_timestamp, ',.4f')}" +
+    " or " +
+    f"{format(unix_timestamp, '.2e')}" +
+    " in scientific notation")
 print(current_datetime.strftime('%b %d %Y'))
