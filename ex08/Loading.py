c5cf65ad416ab7048e2d05c35209f80b6c4e66a0
Alvin Yap <alyap@u91z05s03.42kl.edu.my>
2023-10-02 16:27:03 +0800

    chore: style flake8

--- ex08/Loading.py
+++ ex08/Loading.py
@@ -7,7 +7,8 @@ from config import tqdm_config
 
 def calculate_non_bar_width(i, total, elapsed_time, speed):
     '''
-    Calculate of the width of the bar string that wasn't occupied by progress bar
+    Calculate of the width of the bar string that wasn't occupied
+    by progress bar
     '''
     percentage_width = len(f"{math.floor(i/total * 100)}%")
     item_info_width = len(f" {i}/{total}")
@@ -44,14 +45,14 @@ def ft_tqdm(lst: range) -> None:
                 i, total, elapsed_time, speed)
             n_bars = shutil.get_terminal_size().columns - width_offset
 
-            bar_string = (tqdm_config["opening_char"] +
-                          (tqdm_config["elapsed_char"] * current_bars) +
-                          (tqdm_config["undone_char"] * (n_bars - current_bars)) +
-                          tqdm_config["closing_char"])
+            bar = (tqdm_config["opening_char"] +
+                   (tqdm_config["elapsed_char"] * current_bars) +
+                   ((tqdm_config["undone_char"] * (n_bars - current_bars))) +
+                   tqdm_config["closing_char"])
 
             sys.stdout.write(
                 f"{math.floor(i/total * 100)}%" +
-                bar_string +
+                bar +
                 f" {i}/{total}" +
                 f" [{math.floor(elapsed_time / 60):02}" +
                 f":{(math.floor(elapsed_time) % 60):02}" +
